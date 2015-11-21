f346c078e17f3d2b8a70ad0dd0222bdb3a2e2506
Jerome PL <jerome-pl2@hotmail.ca>
2015-11-20 21:00:51 -0500

    New patient parameters

--- Patient.java
+++ Patient.java
@@ -1,13 +1,24 @@
+import java.util.Arrays;
 
 public class Patient {
 	
 	public int ID;
 	public float[] testResults; //268 data results
 	
-	public Patient(int ID, float[] tests){
-		
+	public boolean resistant;
+	public double remissionDuration;
+	public double survival; //Survival time
+	
+	public Patient(int ID, float[] tests, boolean resistant, double remissionDuration, double survival){
 		this.ID =ID;
-		testResults = tests;
+		this.testResults = tests;
+		this.resistant = resistant;
+		this.remissionDuration = remissionDuration;
+		this.survival = survival;
+	}
+	
+	public String toString() {
+		return "ID: " + ID + ": " + Arrays.toString(testResults);
 	}
 
 }
