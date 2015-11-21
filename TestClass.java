fa0da2dfffb33c59a4c7562b350405cf4c497147
Jerome PL <jerome-pl2@hotmail.ca>
2015-11-20 21:19:08 -0500

    Test class now loads patients properly. Patients have new toString method

--- TestClass.java
+++ TestClass.java
@@ -1,16 +1,99 @@
 import java.io.*;
 import java.util.*;
 
-public class TestClass {
+class TestClass {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		File file = new File("trainingData.txt");
+		ArrayList<Patient> patients = new ArrayList<Patient>();
 		
 		Scanner in = new Scanner(file);
+		in.nextLine(); //Skip the first two lines
+		in.nextLine();
 		
-		while(in.hasNextLine())
-			System.out.println(in.nextLine().split("\\s").length);
+		String[][] input = new String[166][269];
+		
+		for(int i = 0; i < input.length; i++) {
+			input[i] = in.nextLine().split("\\s");
+			
+			int id = Integer.parseInt(input[i][0].substring(input[i][0].length() - 3, input[i][0].length()));
+			float[] finalInput = new float[input[i].length - 4];
+			boolean resistant = false;
+			double remissionDuration = 0;
+			double survival = 0; //Survival time
+			
+			for(int j = 0; j < input[i].length - 1; j++) {
+				char a = input[i][j].charAt(0);
+				if(j < 265 && '0' <= a && a <= '9')
+					finalInput[j] = Float.parseFloat(input[i][j]);
+				else {
+					switch(j) {
+					case 0:
+						finalInput[j] = (input[i][j + 1].charAt(0) == 'F')? 1: 0;
+						break;
+					case 3:
+					case 4:
+					case 5:
+					case 6:
+						char c = input[i][j + 1].charAt(0);
+						if(c == 'Y')
+							finalInput[j] = 1;
+						else if(c == 'N')
+							finalInput[j] = 0;
+						else //TODO NotDone (DNE)
+							finalInput[j] = 0;
+						break;
+					case 7:
+					case 8:
+					case 9:
+						c = input[i][j + 1].charAt(1);
+						if(c == 'O')
+							finalInput[j] = 1;
+						else if(c == 'E')
+							finalInput[j] = 0;
+						else //TODO NotDone (DNE)
+							finalInput[j] = 0;
+						break;
+					case 10:
+						c = input[i][j + 1].charAt(0); //TODO shouldnt be values like 1, 2, 3, 4
+						switch(c) {
+						case 'A':
+							finalInput[j] = 1;
+							break;
+						case 'S':
+							finalInput[j] = 2;
+							break;
+						case 'F':
+							finalInput[j] = 3;
+							break;
+						case 'H':
+							finalInput[j] = 4;
+							break;
+						}
+						break;
+					case 265:
+						resistant = (input[i][j + 1].charAt(0) == 'R')? true: false;
+						break;
+					case 266:
+						if(input[i][j + 1].charAt(0) == 'N')
+							remissionDuration = -1;
+						else
+							remissionDuration = Double.parseDouble(input[i][j + 1]);
+						break;
+					case 267:
+						survival = Double.parseDouble(input[i][j + 1]);
+						break;
+					}
+				}
+			}
+
+			patients.add(new Patient(id, finalInput, resistant, remissionDuration, survival));
+		}
+		
+		for(Patient p: patients) {
+			System.out.println(p);
+		}
 
 	}
 
